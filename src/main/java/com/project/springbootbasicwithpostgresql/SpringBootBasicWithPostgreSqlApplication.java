edfc893c647999262f87a4bee63ce626e46b4d05
nay-eem-01 <ahmedmdnayeem05@gmail.com>
2025-08-13 04:08:07 +0600

    Initial commit

---
+++ src/main/java/com/project/springbootbasicwithpostgresql/SpringBootBasicWithPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.project.springbootbasicwithpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicWithPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicWithPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springbootbasicwithpostgresql/SpringBootBasicWithPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springbootbasicwithpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicWithPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
